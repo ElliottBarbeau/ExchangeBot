f5b800acc91e45c328d9a526b10f5dc7b818979d
ElliottBarbeau <elliottbarbeau@gmail.com>
2025-07-19 10:14:24 -0400

    Added Cassandra security to move bot to vps

--- exchange-bot/database/cassandra_client.py
+++ exchange-bot/database/cassandra_client.py
@@ -1,13 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+from pathlib import Path
 from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 
-cluster = Cluster(["127.0.0.1"], port=9042)
-session = cluster.connect()
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE = ".env"
 
-session.execute("""
-            CREATE KEYSPACE IF NOT EXISTS exchangebot
-            WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'}
-        """)
+load_dotenv(BASE_DIR / ENV_FILE)
 
-session.set_keyspace('exchangebot')
+CASSANDRA_KEYSPACE = os.getenv('CASSANDRA_KEYSPACE')
+CASSANDRA_PASSWORD = os.getenv('CASSANDRA_PASSWORD')
+CASSANDRA_USER = os.getenv('CASSANDRA_USER')
+CASSANDRA_PORT = int(os.getenv("CASSANDRA_PORT", 9042))
+CASSANDRA_HOST = os.getenv('CASSANDRA_HOST')
 
-print(f"[Cassandra] Connected to keyspace: exchangebot")
+auth_provider = PlainTextAuthProvider(username=CASSANDRA_USER, password=CASSANDRA_PASSWORD)
+cluster = Cluster([CASSANDRA_HOST], port=CASSANDRA_PORT, auth_provider=auth_provider)
+session = cluster.connect(CASSANDRA_KEYSPACE)
+session.set_keyspace(CASSANDRA_KEYSPACE)
+
+print(f"[Cassandra] Connected to keyspace: {CASSANDRA_KEYSPACE}")
